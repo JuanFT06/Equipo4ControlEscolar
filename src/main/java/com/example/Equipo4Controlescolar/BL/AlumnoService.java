c4ef42aaea61465128e559c19cb0b87b14b72822
digis <digis@ALIEN6>
2023-12-13 16:25:11 -0600

    ServiciosAlumnos

--- src/main/java/com/example/Equipo4Controlescolar/BL/AlumnoService.java
+++ src/main/java/com/example/Equipo4Controlescolar/BL/AlumnoService.java
@@ -4,10 +4,51 @@
  */
 package com.example.Equipo4Controlescolar.BL;
 
+import com.example.Equipo4Controlescolar.DL.Alumno;
+import com.example.Equipo4Controlescolar.DL.AlumnoRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  *
  * @author digis
  */
+@Service
 public class AlumnoService {
-    
+
+    private AlumnoRepository alumnoRepository;
+
+    @Autowired
+    public AlumnoService(AlumnoRepository alumnoRepository) {
+        this.alumnoRepository = alumnoRepository;
+    }
+
+    public List<Alumno> findall() {
+        return alumnoRepository.findAll();
+    }
+
+    public Alumno saveAlumno(Alumno alumno) {
+        return alumnoRepository.save(alumno);
+    }
+
+    public Alumno updateAlumno(Alumno alumno, int alumnoId) {
+        Optional<Alumno> optionalAlumno = alumnoRepository.findById(alumnoId);
+
+        if (optionalAlumno.isPresent()) {
+            Alumno alumnoDB = optionalAlumno.get();
+            alumnoDB.setNombre(alumno.getNombre());
+            alumnoDB.setApellidopaterno(alumno.getApellidopaterno());
+            alumnoDB.setApellidomaterno(alumno.getApellidomaterno());
+            return alumnoRepository.save(alumnoDB);
+        } else {
+
+            return null;
+        }
+
+}
+    public void deleteDepartmentById(int alumnoId) {
+        alumnoRepository.deleteById(alumnoId);
+    }
 }
--- src/main/java/com/example/Equipo4Controlescolar/DL/Alumno.java
+++ src/main/java/com/example/Equipo4Controlescolar/DL/Alumno.java
@@ -4,10 +4,68 @@
  */
 package com.example.Equipo4Controlescolar.DL;
 
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 /**
  *
  * @author digis
  */
+@Entity
 public class Alumno {
+    @Id
+    private int idalumno;
+    @Basic
+    private String nombre;
+    private String apellidopaterno;
+    private String apellidomaterno;
+
+    public Alumno() {
+    }
+
+    public Alumno(int idalumno, String nombre, String apellidopaterno, String apellidomaterno) {
+        this.idalumno = idalumno;
+        this.nombre = nombre;
+        this.apellidopaterno = apellidopaterno;
+        this.apellidomaterno = apellidomaterno;
+    }
+
+    public int getIdalumno() {
+        return idalumno;
+    }
+
+    public void setIdalumno(int idalumno) {
+        this.idalumno = idalumno;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidopaterno() {
+        return apellidopaterno;
+    }
+
+    public void setApellidopaterno(String apellidopaterno) {
+        this.apellidopaterno = apellidopaterno;
+    }
+
+    public String getApellidomaterno() {
+        return apellidomaterno;
+    }
+
+    public void setApellidomaterno(String apellidomaterno) {
+        this.apellidomaterno = apellidomaterno;
+    }
+    
+    
+    
+    
+    
     
 }
