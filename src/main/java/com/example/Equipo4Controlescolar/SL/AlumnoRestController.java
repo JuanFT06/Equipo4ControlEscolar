6f1d290fb40e55499f653ef2e5a7b8a668ceae5d
digis <digis@ALIEN6>
2023-12-15 13:41:54 -0600

    ServiciosAlumnos

--- src/main/java/com/example/Equipo4Controlescolar/SL/AlumnoRestController.java
+++ src/main/java/com/example/Equipo4Controlescolar/SL/AlumnoRestController.java
@@ -6,16 +6,12 @@ package com.example.Equipo4Controlescolar.SL;
 
 import com.example.Equipo4Controlescolar.BL.AlumnoService;
 import com.example.Equipo4Controlescolar.DL.Alumno;
-import com.example.Equipo4Controlescolar.DL.AlumnoRepository;
-import jakarta.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,8 +43,9 @@ public class AlumnoRestController {
         return alumnoService.saveAlumno(alumno);
     }
 
-    @PostMapping("/Update")
-    public Alumno updateDepartment(@RequestBody Alumno alumno) {
+    @PostMapping("/Update/{IdAlumno}")
+    public Alumno updateDepartment(@PathVariable int IdAlumno, @RequestBody Alumno alumno) {
+        alumno.setIdalumno(IdAlumno);
         return alumnoService.saveAlumno(alumno);
     }
 
