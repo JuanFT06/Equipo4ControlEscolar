f4c87c9aa320b9f9f7615aac21cee5595b2be486
digis <digis@ALIEN3P9>
2023-12-18 17:42:41 -0600

    proyecto completo

--- src/main/java/com/example/Equipo4Controlescolar/SL/MateriaSPRestController.java
+++ src/main/java/com/example/Equipo4Controlescolar/SL/MateriaSPRestController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +35,7 @@ public class MateriaSPRestController {
         return materiaService.saveMateriasP(materia);
     }
     
-    @PostMapping("/updatesp/{idmateria}")
+    @PutMapping("/updatesp/{idmateria}")
     public String updateMateriaSP(@PathVariable int idmateria, @RequestBody Materia materia){
         materia.setIdmateria(idmateria);
         return materiaService.updateMateriaSP(materia);
